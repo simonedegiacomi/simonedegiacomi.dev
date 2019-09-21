3fc06b10de0556ca80a148dfa1451572b522d0c3
simonedegiacomi <simonedegiacomi97@gmail.com>
2019-09-21 22:27:33 +0200

    update style

--- src/Components/Editor/EditorHeader/EditorHeader.tsx
+++ src/Components/Editor/EditorHeader/EditorHeader.tsx
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 
 import "./EditorHeader.css";
 import Toggle from "./Toggle";
+import MobileMenuToggleTriggerIcon from "../../../icons/mobile-menu.svg";
 
 
 export default class EditorHeader extends Component<EditorHeaderProps> {
@@ -10,8 +11,10 @@ export default class EditorHeader extends Component<EditorHeaderProps> {
         return (
             <div className="editor-header">
                 <div className="mobile-menu-toggle-trigger">
-                    <Toggle onClick={this.props.onMobileMenuToggled}
-                            mobileMenuOpen={this.props.mobileMenuOpen}/>
+                    <img src={MobileMenuToggleTriggerIcon} onClick={this.props.onMobileMenuToggled}/>
+
+                    {/*<Toggle onClick={this.props.onMobileMenuToggled}*/}
+                    {/*        mobileMenuOpen={this.props.mobileMenuOpen}/>*/}
                 </div>
                 <div style={{flexGrow: 1}}>simonedegiacomi.dev</div>
             </div>
