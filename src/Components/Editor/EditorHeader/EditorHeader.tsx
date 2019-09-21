6231e16681f1f722f09953e1705cb0ef252f86ed
simonedegiacomi <simonedegiacomi97@gmail.com>
2019-09-21 23:39:26 +0200

    improve sidebar toggle

--- src/Components/Editor/EditorHeader/EditorHeader.tsx
+++ src/Components/Editor/EditorHeader/EditorHeader.tsx
@@ -2,8 +2,6 @@ import React, {Component} from "react";
 
 import "./EditorHeader.css";
 import Toggle from "./Toggle";
-import MobileMenuToggleTriggerIcon from "../../../icons/mobile-menu.svg";
-
 
 export default class EditorHeader extends Component<EditorHeaderProps> {
 
@@ -11,10 +9,8 @@ export default class EditorHeader extends Component<EditorHeaderProps> {
         return (
             <div className="editor-header">
                 <div className="mobile-menu-toggle-trigger">
-                    <img src={MobileMenuToggleTriggerIcon} onClick={this.props.onMobileMenuToggled}/>
-
-                    {/*<Toggle onClick={this.props.onMobileMenuToggled}*/}
-                    {/*        mobileMenuOpen={this.props.mobileMenuOpen}/>*/}
+                    <Toggle onClick={this.props.onMobileMenuToggled}
+                            mobileMenuOpen={this.props.mobileMenuOpen}/>
                 </div>
                 <div style={{flexGrow: 1}}>simonedegiacomi.dev</div>
             </div>
--- src/Components/Editor/EditorHeader/Toggle.tsx
+++ src/Components/Editor/EditorHeader/Toggle.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import "./Toggle.css";
+import classNames from "classnames";
 
 const Toggle = ({onClick, mobileMenuOpen }: ToggleProps) => (
-    <div onClick={onClick}>
-        Toggle {mobileMenuOpen}
+    <div onClick={onClick} className={getClassName(mobileMenuOpen)}>
+        <span/>
+        <span/>
+        <span/>
     </div>
 );
+
+function getClassName (mobileMenuOpen: boolean) {
+    return classNames({
+       toggle:true,
+       open: mobileMenuOpen
+    });
+}
 
 interface ToggleProps {
     onClick: () => void,
